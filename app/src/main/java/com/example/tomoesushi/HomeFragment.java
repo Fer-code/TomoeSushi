11a2792fc48f4f688c3182a54bdc51f246c7163f
Fer-code <fernandamirandacursos@gmail.com>
2021-07-23 17:57:31 -0300

    fragment

---
+++ app/src/main/java/com/example/tomoesushi/HomeFragment.java
@@ -0,0 +1,42 @@
+package com.example.tomoesushi;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
+
+public class HomeFragment extends Fragment {
+    private int[] mImages = new int[]{
+            R.drawable.prato_apres_1,
+            R.drawable.prato_apres_2,
+            R.drawable.prato_apres_3,
+            R.drawable.prato_apres_4
+    };
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //return inflater.inflate(R.layout.fragment_home, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        CarouselView carouselView = (CarouselView) view.findViewById(R.id.carousel_view);
+
+        carouselView.setPageCount(mImages.length);
+        carouselView.setImageListener(new ImageListener() {
+            @Override
+            public void setImageForPosition(int position, ImageView imageView) {
+                imageView.setImageResource(mImages[position]);
+            }
+        });
+        return view;
+    }
+
+}
--- app/src/main/java/com/example/tomoesushi/MainActivity.java
+++ app/src/main/java/com/example/tomoesushi/MainActivity.java
@@ -1,15 +1,59 @@
 package com.example.tomoesushi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageClickListener;
+import com.synnapps.carouselview.ImageListener;
+
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
+
+        BottomNavigationView bottomNav = findViewById(R.id.nav_view);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
+
+
     }
-}
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener=
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment fragSelecionada = null;
+
+                    switch (item.getItemId()){
+
+                        case R.id.nav_pedido:
+                            fragSelecionada = new PedidoFragment();
+                            break;
+
+                        case R.id.nav_home:
+                            fragSelecionada = new HomeFragment();
+                            break;
+
+                        case R.id.nav_pesquisar:
+                            fragSelecionada= new PesquisaFragment();
+                            break;
+
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            fragSelecionada).commit();
+
+                    return true;
+                }
+            };
+    }
